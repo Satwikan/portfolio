7623bc651ce18c274e31329f7eecdaf022aca5ed
Satwikan <satwikanmol25@gmail.com>
2021-12-15 19:24:01 +0530

    dark mode color added

--- layouts/Navbar.js
+++ layouts/Navbar.js
@@ -28,7 +28,7 @@ export default function Navbar() {
       {({ open }) => (
         <>
           {/* Mobile options and whole large screen */}
-          <div className="max-w-6xl mx-auto px-4 pt-10 md:py-20">
+          <div className="max-w-6xl mx-auto px-4 pt-10 md:pt-20">
             <div className="relative flex items-center justify-between">
               {/* Mobile menu button*/}
               <div className="absolute top-0 left-0 sm:hidden">
@@ -60,7 +60,7 @@ export default function Navbar() {
                       <a
                         key={item.name}
                         href={item.href}
-                        className="px-3 py-2 rounded-md text-lg font-medium"
+                        className="px-3 py-2 rounded-md text-lg font-medium hover:underline"
                         aria-current={item.current ? "page" : undefined}
                       >
                         {item.name}
@@ -142,29 +142,37 @@ export default function Navbar() {
                 className="w-10 h-10 p-3 rounded focus:outline-none"
                 onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  fill="currentColor"
-                  stroke="currentColor"
-                  className="w-6 h-6 text-yellow-500 dark:text-yellow-500"
-                >
-                  {theme === "dark" ? (
+                {theme === "dark" ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="yellow"
+                    stroke="yellow"
+                    className="w-6 h-6"
+                  >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
                       d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
                     />
-                  ) : (
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    fill="current"
+                    stroke="current"
+                    className="w-6 h-6"
+                  >
                     <path
                       strokeLinecap="round"
                       strokeLinejoin="round"
                       strokeWidth={2}
                       d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
                     />
-                  )}
-                </svg>
+                  </svg>
+                )}
               </button>
             </div>
           </div>
