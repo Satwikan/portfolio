5d7021721bd80d6aee6309c0663922832712ad56
Satwikan <69719072+Satwikan@users.noreply.github.com>
2022-05-13 02:34:46 -0700

    Update data.js

--- constants/data.js
+++ constants/data.js
@@ -99,7 +99,7 @@ const userData = {
     },
   ],
   resumeUrl:
-    "https://drive.google.com/file/d/13q6nacONU-wygawASDluVdcDIotXOhFi/view?usp=sharing",
+    "https://drive.google.com/file/d/17oMLzKfYhMK2IJeWEvrGkiLaR0EsyRUe/view?usp=sharing",
   socialLinks: {
     twitter: "https://twitter.com/satwikum",
     linkedin: "https://www.linkedin.com/in/satwik-anmol-1487a3191/",
